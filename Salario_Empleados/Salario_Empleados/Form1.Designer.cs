0006f115c8c8707267837856666f7fd23446f732
Diego <diegoayala1232@hotmail.com>
2021-09-07 16:19:22 -0600

    Ejercicio 1, version final

--- Salario_Empleados/Salario_Empleados/Form1.Designer.cs
+++ Salario_Empleados/Salario_Empleados/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Salario_Empleados
 {
-    partial class Form1
+    partial class frm_empleados
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,13 +29,191 @@ namespace Salario_Empleados
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.lb_nombre = new System.Windows.Forms.Label();
+            this.lb_apellido = new System.Windows.Forms.Label();
+            this.lb_bruto = new System.Windows.Forms.Label();
+            this.lb_descuento = new System.Windows.Forms.Label();
+            this.lb_salario = new System.Windows.Forms.Label();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_bruto = new System.Windows.Forms.TextBox();
+            this.txt_apellido = new System.Windows.Forms.TextBox();
+            this.lbr_descuento = new System.Windows.Forms.Label();
+            this.lbr_neto = new System.Windows.Forms.Label();
+            this.rbt_gerente = new System.Windows.Forms.RadioButton();
+            this.rbt_subgerente = new System.Windows.Forms.RadioButton();
+            this.rbt_secretaria = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(239, 363);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(133, 38);
+            this.btn_calcular.TabIndex = 0;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lb_nombre
+            // 
+            this.lb_nombre.AutoSize = true;
+            this.lb_nombre.Location = new System.Drawing.Point(68, 53);
+            this.lb_nombre.Name = "lb_nombre";
+            this.lb_nombre.Size = new System.Drawing.Size(67, 20);
+            this.lb_nombre.TabIndex = 1;
+            this.lb_nombre.Text = "Nombre:";
+            // 
+            // lb_apellido
+            // 
+            this.lb_apellido.AutoSize = true;
+            this.lb_apellido.Location = new System.Drawing.Point(358, 53);
+            this.lb_apellido.Name = "lb_apellido";
+            this.lb_apellido.Size = new System.Drawing.Size(79, 20);
+            this.lb_apellido.TabIndex = 2;
+            this.lb_apellido.Text = "Apellidos: ";
+            // 
+            // lb_bruto
+            // 
+            this.lb_bruto.AutoSize = true;
+            this.lb_bruto.Location = new System.Drawing.Point(79, 136);
+            this.lb_bruto.Name = "lb_bruto";
+            this.lb_bruto.Size = new System.Drawing.Size(98, 20);
+            this.lb_bruto.TabIndex = 3;
+            this.lb_bruto.Text = "Salario bruto:";
+            // 
+            // lb_descuento
+            // 
+            this.lb_descuento.AutoSize = true;
+            this.lb_descuento.Location = new System.Drawing.Point(55, 201);
+            this.lb_descuento.Name = "lb_descuento";
+            this.lb_descuento.Size = new System.Drawing.Size(132, 20);
+            this.lb_descuento.TabIndex = 4;
+            this.lb_descuento.Text = "Monto descuento: ";
+            // 
+            // lb_salario
+            // 
+            this.lb_salario.AutoSize = true;
+            this.lb_salario.Location = new System.Drawing.Point(79, 269);
+            this.lb_salario.Name = "lb_salario";
+            this.lb_salario.Size = new System.Drawing.Size(92, 20);
+            this.lb_salario.TabIndex = 5;
+            this.lb_salario.Text = "Salario neto:";
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(154, 50);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(154, 27);
+            this.txt_nombre.TabIndex = 6;
+            // 
+            // txt_bruto
+            // 
+            this.txt_bruto.Location = new System.Drawing.Point(195, 133);
+            this.txt_bruto.Name = "txt_bruto";
+            this.txt_bruto.Size = new System.Drawing.Size(125, 27);
+            this.txt_bruto.TabIndex = 7;
+            // 
+            // txt_apellido
+            // 
+            this.txt_apellido.Location = new System.Drawing.Point(455, 53);
+            this.txt_apellido.Name = "txt_apellido";
+            this.txt_apellido.Size = new System.Drawing.Size(234, 27);
+            this.txt_apellido.TabIndex = 8;
+            // 
+            // lbr_descuento
+            // 
+            this.lbr_descuento.AutoSize = true;
+            this.lbr_descuento.Location = new System.Drawing.Point(195, 201);
+            this.lbr_descuento.Name = "lbr_descuento";
+            this.lbr_descuento.Size = new System.Drawing.Size(18, 20);
+            this.lbr_descuento.TabIndex = 9;
+            this.lbr_descuento.Text = "...";
+            // 
+            // lbr_neto
+            // 
+            this.lbr_neto.AutoSize = true;
+            this.lbr_neto.Location = new System.Drawing.Point(195, 269);
+            this.lbr_neto.Name = "lbr_neto";
+            this.lbr_neto.Size = new System.Drawing.Size(18, 20);
+            this.lbr_neto.TabIndex = 10;
+            this.lbr_neto.Text = "...";
+            // 
+            // rbt_gerente
+            // 
+            this.rbt_gerente.AutoSize = true;
+            this.rbt_gerente.Location = new System.Drawing.Point(528, 148);
+            this.rbt_gerente.Name = "rbt_gerente";
+            this.rbt_gerente.Size = new System.Drawing.Size(82, 24);
+            this.rbt_gerente.TabIndex = 11;
+            this.rbt_gerente.TabStop = true;
+            this.rbt_gerente.Text = "Gerente";
+            this.rbt_gerente.UseVisualStyleBackColor = true;
+            // 
+            // rbt_subgerente
+            // 
+            this.rbt_subgerente.AutoSize = true;
+            this.rbt_subgerente.Location = new System.Drawing.Point(528, 197);
+            this.rbt_subgerente.Name = "rbt_subgerente";
+            this.rbt_subgerente.Size = new System.Drawing.Size(106, 24);
+            this.rbt_subgerente.TabIndex = 12;
+            this.rbt_subgerente.TabStop = true;
+            this.rbt_subgerente.Text = "Subgerente";
+            this.rbt_subgerente.UseVisualStyleBackColor = true;
+            // 
+            // rbt_secretaria
+            // 
+            this.rbt_secretaria.AutoSize = true;
+            this.rbt_secretaria.Location = new System.Drawing.Point(528, 249);
+            this.rbt_secretaria.Name = "rbt_secretaria";
+            this.rbt_secretaria.Size = new System.Drawing.Size(96, 24);
+            this.rbt_secretaria.TabIndex = 13;
+            this.rbt_secretaria.TabStop = true;
+            this.rbt_secretaria.Text = "Secretaria";
+            this.rbt_secretaria.UseVisualStyleBackColor = true;
+            // 
+            // frm_empleados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(831, 476);
+            this.Controls.Add(this.rbt_secretaria);
+            this.Controls.Add(this.rbt_subgerente);
+            this.Controls.Add(this.rbt_gerente);
+            this.Controls.Add(this.lbr_neto);
+            this.Controls.Add(this.lbr_descuento);
+            this.Controls.Add(this.txt_apellido);
+            this.Controls.Add(this.txt_bruto);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.lb_salario);
+            this.Controls.Add(this.lb_descuento);
+            this.Controls.Add(this.lb_bruto);
+            this.Controls.Add(this.lb_apellido);
+            this.Controls.Add(this.lb_nombre);
+            this.Controls.Add(this.btn_calcular);
+            this.Name = "frm_empleados";
+            this.Text = "Ventana de empleados";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Label lb_nombre;
+        private System.Windows.Forms.Label lb_apellido;
+        private System.Windows.Forms.Label lb_bruto;
+        private System.Windows.Forms.Label lb_descuento;
+        private System.Windows.Forms.Label lb_salario;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txt_bruto;
+        private System.Windows.Forms.TextBox txt_apellido;
+        private System.Windows.Forms.Label lbr_descuento;
+        private System.Windows.Forms.Label lbr_neto;
+        private System.Windows.Forms.RadioButton rbt_gerente;
+        private System.Windows.Forms.RadioButton rbt_subgerente;
+        private System.Windows.Forms.RadioButton rbt_secretaria;
     }
 }
 
--- Salario_Empleados/Salario_Empleados/Form1.cs
+++ Salario_Empleados/Salario_Empleados/Form1.cs
@@ -10,12 +10,87 @@ using System.Windows.Forms;
 
 namespace Salario_Empleados
 {
-    public partial class Form1 : Form
+    
+    public partial class frm_empleados : Form
     {
-        public Form1()
+        public frm_empleados()
         {
             InitializeComponent();
         }
 
-    }
-}
+        private void limpiar()
+        {
+            txt_nombre.Clear();
+            txt_apellido.Clear();
+            txt_bruto.Clear();
+            rbt_gerente.Checked = false;
+            rbt_subgerente.Checked = false;
+            rbt_secretaria.Checked = false;
+        }//fin funcion limpiar
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombre, apellido;
+            double s_bruto, descuento=0, s_neto=0;
+            int cargo =0;
+
+            //captura de datos
+            nombre = txt_nombre.Text;
+            apellido = txt_apellido.Text;
+            s_bruto = Convert.ToDouble(txt_bruto.Text);
+
+            //validacion de datos
+            if(nombre == "" || apellido == "")
+            {
+                MessageBox.Show("No puede dejar campos vacios, por favor reingrese los datos");
+                limpiar();
+            }
+            if (s_bruto <= 0)
+            {
+                MessageBox.Show("No puede ingresar salario negativo o dejar el campo vacio, por favor reingrese los datos");
+                limpiar();
+            }
+
+            if (rbt_gerente.Checked)
+            {
+                cargo = 1;
+            }
+            else if (rbt_subgerente.Checked)
+            {
+                cargo = 2;
+            }
+            else if (rbt_secretaria.Checked)
+            {
+                cargo = 3;
+            }
+            else
+            {
+                MessageBox.Show("Debe de seleccionar un cargo, por favor reingrese los datos");
+                limpiar();
+            }
+
+            //proceso de calculo
+            switch (cargo)
+            {
+                case 1:
+                    descuento = s_bruto * .20;
+                    s_neto = s_bruto - descuento;
+                    break;
+                case 2:
+                    descuento = s_bruto * .15;
+                    s_neto = s_bruto - descuento;
+                    break;
+                case 3:
+                    descuento = s_bruto * .5;
+                    s_neto = s_bruto - descuento;
+                    break;
+            }//fin switch
+
+            //impresion de resultados
+            lbr_descuento.Text = "$" + descuento;
+            lbr_neto.Text = "$" + s_neto;
+        }
+    }//fin funcion cilck calcular
+   
+}//fin programa
+
